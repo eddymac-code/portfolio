be33a5bc7e301ae16d9032c32e0f143a377085aa
Eddy Mac <eddymac11@outlook.com>
2020-03-18 18:04:46 +0300

    some js, and total responsiveness

--- js/content.js
+++ js/content.js
@@ -22,3 +22,13 @@ $('a[href*="#"]')
       }
     }
   });
+
+  /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function lilMenu() {
+  var x = document.getElementById("nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
